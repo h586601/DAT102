a52014f5fd8f2d45c36676a113778c79e4ddfa56
h586601 <586601@stud.hvl.no>
2020-02-12 14:32:58 +0100

    Javadoc for Filmarkiv Interface

--- Exercise 1/src/no/hvl/data102/adt/FilmarkivADT.java
+++ Exercise 1/src/no/hvl/data102/adt/FilmarkivADT.java
@@ -4,25 +4,50 @@ import no.hvl.data102.*;
 
 public interface FilmarkivADT {
 
-	//Return an array of films
+	/**
+	 * Return an array of films
+	 * @return Film[]
+	 */
 	Film[] hentFilmTabell();
 	
-	//Add a new movie
+	/**
+	 * Add a new movie
+	 * @param nyFilm
+	 */
 	void leggTilFilm(Film nyFilm);
 	
-	//Delete a movie if it exists 
+	/**
+	 * Delete a movie if it exists 
+	 * @param filmnr
+	 * @return boolean -> true if element deleted and false if not
+	 */
 	boolean slettFilm(int filmnr);
 	
-	//Search and collect movies that include a certain string
+	/**
+	 * Search and collect movies that include a certain string
+	 * @param delstreng
+	 * @return Film[] 
+	 */
 	Film[] sokTittel(String delstreng);
 	
-	//Search and collect producers that include a certain string/keyword
+	/**
+	 * Search and collect producers that include a certain string/keyword
+	 * @param delstreng
+	 * @return Film[]
+	 */
 	Film[] sokProdusent(String delstreng);
 	
-	//Count number of movies within a given genre 
+	/**
+	 * Count number of movies within a given genre 
+	 * @param sjanger
+	 * @return integer
+	 */
 	int antallSjanger(Sjanger sjanger);
 
-	//Return number of movies
+	/**
+	 * Return number of movies
+	 * @return integer
+	 */
 	int antall();
 	
-} //interface
+}
