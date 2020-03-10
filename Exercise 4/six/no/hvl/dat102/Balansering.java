508cbf69b49991e9f0ec7aab3854e25eeca17c4a
h586601 <586601@stud.hvl.no>
2020-03-10 14:16:42 +0100

    Oppgave 6

---
+++ Exercise 4/six/no/hvl/dat102/Balansering.java
@@ -0,0 +1,101 @@
+package no.hvl.dat102;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import no.hvl.dat102.adt.StabelADT;
+
+public class Balansering {
+	StabelADT<Parentesinfo> stabel = new TabellStabel<Parentesinfo>();
+	boolean feil = true;
+
+	private boolean passer(char åpent, char lukket) {
+		switch (åpent) {
+		case '(':
+			return (lukket == ')');
+		case '[':
+			return (lukket == ']');
+		case '{':
+			return (lukket == '}');
+		default:
+			return false;
+		}
+	}
+
+	public void foretaBalansering(String innDataStreng, int linjenr) {
+
+		int lengde = innDataStreng.length();
+
+
+		for (int i = 0; i < lengde; i++) {
+
+			char parentes = innDataStreng.charAt(i);
+
+			if(parentes == '(' || parentes == '{'|| parentes == '[') {
+				Parentesinfo pinfo = new Parentesinfo(linjenr, i, parentes);
+				stabel.push(pinfo);
+			} else if (parentes == ')' || parentes == '}'|| parentes == ']') {
+				if (stabel.erTom()) {
+					feil = false;
+					System.out.println(new Parentesinfo(linjenr, i, parentes).toStringTomStabel());
+				} else {
+					Parentesinfo poppet = stabel.pop();
+					if ( !passer(poppet.getVenstreparentes(),parentes ) ) {
+						feil= false;
+						System.out.println(poppet.toStringIkkeBalansert());
+					}
+				}
+			}
+		}
+	}
+
+	public void lesFraFil(String filnavn) {
+		FileReader tekstFilLeser = null;
+		try {
+			tekstFilLeser = new FileReader(filnavn);
+		} catch (FileNotFoundException unntak) {
+			System.out.println("Finner ikke filen!");
+			System.exit(-1);
+		}
+
+		BufferedReader tekstLeser = new BufferedReader(tekstFilLeser);
+		String linje = null;
+		int linjenr = 0;
+		try {
+			linje = tekstLeser.readLine();
+			while (linje != null) {
+				foretaBalansering(linje, linjenr);
+				linje = tekstLeser.readLine();
+				linjenr++;
+			}
+
+			if (!stabel.erTom()) {
+				System.out.println("Stabelen er ikke tom etter innlesing.");
+				while (!stabel.erTom()) {
+					feil = false;
+					Parentesinfo rest = stabel.pop();
+					System.out.println(rest.toStringRest());
+				}
+			}
+			if (feil == true) {
+				System.out.println("Filen er balansert.");
+			} else {
+				System.out.println("Filen er ikke balansert.");
+			}
+		}
+
+		catch (IOException unntak) {
+			System.out.println("Feil ved innlesing!");
+			System.exit(-1);
+		}
+		try {
+			tekstFilLeser.close();
+		} catch (IOException unntak) {
+			System.out.println("Feil ved lukking av fil!");
+		}
+
+	}
+
+}
