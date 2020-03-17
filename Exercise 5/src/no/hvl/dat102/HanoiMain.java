95b4522a73f4ed08574e7b8a90c92cf3ccbc08fe
h586601 <586601@stud.hvl.no>
2020-03-18 00:12:24 +0100

    HanoiMain, plus leggTil() and fjern() in DobbelKjedetOrdnetListe

--- Exercise 5/src/no/hvl/dat102/HanoiMain.java
+++ Exercise 5/src/no/hvl/dat102/HanoiMain.java
@@ -8,6 +8,8 @@ public class HanoiMain {
 	public static void main(String[] args) {
 
 		Hanoi tårn = new Hanoi(28);
+		Hanoi tårn2 = new Hanoi(29);
+		Hanoi tårn3 = new Hanoi(31);
 
 		Instant start = Instant.now();
 
@@ -17,6 +19,21 @@ public class HanoiMain {
 		System.out.println(Duration.between(start, finish).toMillis());
 
 
+		Instant start2 = Instant.now();
+
+		tårn2.spill();
+
+		Instant finish2 = Instant.now();
+		System.out.println(Duration.between(start2, finish2).toMillis());
+
+		Instant start3 = Instant.now();
+
+		tårn3.spill();
+
+		Instant finish3 = Instant.now();
+		System.out.println(Duration.between(start3, finish3).toMillis());
+
+		System.out.println(Math.pow(2, 28));
 	}
 
 }
